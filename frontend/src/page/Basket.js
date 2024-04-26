b56ecf1ddde9234ae992526073302bce01522e54
Wonwoo Park <gn030108@gmail.com>
2024-04-27 01:21:53 +0900

    장바구니 css 수정 1차

--- frontend/src/page/Basket.js
+++ frontend/src/page/Basket.js
@@ -17,10 +17,10 @@ const Basket = () => {
 
   //체크리스트 데이터 가정
   const items = [
-    {id:1,img:김민주,name:'김민주사진',option1:'M',option2:'린넨', price :'10000', count:'2'},
-    // {id:2,img:김민주,name:'김민주사진',option1:'M',option2:'린넨', price :'10000', count:'2'},
-    // {id:3,img:김민주,name:'김민주사진',option1:'M',option2:'린넨', price :'10000', count:'2'},
-    // {id:4,img:김민주,name:'김민주사진',option1:'M',option2:'린넨', price :'10000', count:'2'},
+    {img:김민주,name:'김민주사진',option1:'M',option2:'린넨', price :'10000', count:'2'},
+    {img:김민주,name:'김민주사진',option1:'M',option2:'린넨', price :'10000', count:'2'},
+    {img:김민주,name:'김민주사진',option1:'M',option2:'린넨', price :'10000', count:'2'},
+    {img:김민주,name:'김민주사진',option1:'M',option2:'린넨', price :'10000', count:'2'},
   ]
 
   const checkItems = useSelector((state)=>state.basket.checkItems)
@@ -71,10 +71,72 @@ const Basket = () => {
           </div>
           {/* 장바구니 내역 */}
           <div className={styles.basket_box}>
-            {/* 내역 종류 */}
-            <div className={styles.option_box}>
+             {/* 전체x개 상품명(옵션) 판매가 수량 주문금액 주문관리(삭제버튼) */}
+            <div className={styles.header_box}>
+              <div>전체 x개</div>
+              <input type='checkbox' name='select-all'
+                onChange={(e) => handleAllCheck(e.target.checked)}
+                // 데이터 개수와 체크된 아이템의 개수가 다를 경우 선택 해제 (하나라도 해제 시 선택 해제)
+                checked={checkItems.length === items.length ? true : false} />
+              <div>상품명(옵션)</div>
+              <div>판매가</div>
+              <div>수량</div>
+              <div>주문금액</div>
+              <div>주문관리</div>
+            </div>
+            {/* 장바구니 아이탬 */}
+            {items.map((item,index)=>(
+              <div className={styles.content_box}>
+                <div>{index+1}</div>
+                <input 
+                  type='checkbox' name={`select-${item.id}`}
+                  onChange={(e) => handleSingleCheck(e.target.checked, index+1)}
+                  // 체크된 아이템 배열에 해당 아이템이 있을 경우 선택 활성화, 아닐 시 해제, 장바구니에서 미리 선택했던 상품에 대해 체크를 해두기 위함
+                  checked={checkItems.includes(item.id) ? true : false} 
+                />
+                <div>
+                  <div className={styles.item_imgBox}>
+                    <img src={item.img}/>
+                  </div>
+                  <div className={styles.item_info}>
+                    <p>[무신사스텐다드]{item.name}</p>
+                    <p>옵션 : {item.option1} / {item.option2}</p>
+                  </div>
+                </div>
+                <div>{item.price}원</div>
+                <div>{item.count}개</div>
+                <div>{item.price * item.count}원</div>
+                <div className={styles.delete_btn}>
+                  <button>삭제</button>
+                </div>
+                
+              </div>
+            ))}
+
+
+          </div>
+          {/* 주문하기버튼 */}
+          <div className={styles.button_box}>
+            <button onClick={()=>{setPaymentModal(!paymentModal)}}>
+              주문하기
+            </button>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    </>
+    
+  )
+}
+
+export default Basket
+
+
+
+{/* 내역 종류 */}
+            {/* <div className={styles.option_box}>
               {/* 전체x개 상품명(옵션) 판매가 수량 주문금액 주문관리(삭제버튼) */}
-              <span>전체{items.length}개</span>
+              {/* <span>전체{items.length}개</span>
               <input type='checkbox' name='select-all'
                 onChange={(e) => handleAllCheck(e.target.checked)}
                 // 데이터 개수와 체크된 아이템의 개수가 다를 경우 선택 해제 (하나라도 해제 시 선택 해제)
@@ -84,14 +146,15 @@ const Basket = () => {
               <span>수량</span>
               <span>주문금액</span>
               <span>주문관리</span>
-            </div>
+            </div> */}
 
 
+            
             {/* const items = [
                           {itemImg:{김민주},itemName:'김민주사진',option:M, cost :'10000', count:'2', totalCost:cost*count,}
                             ] */}
             {/* 장바구니 내용 */}
-            {items.map((item,index)=>(
+            {/* {items.map((item,index)=>(
               <div className={styles.content_box} key={index}>
                 <span>{item.id}</span>
                 <input 
@@ -114,48 +177,4 @@ const Basket = () => {
                   <button>삭제</button>
                 </span>
             </div>
-            ))}
-            {/* {items.map((item, index)=>(
-              <div className={styles.content_box} key={index}>
-              <span>{item.id}</span>
-              <input 
-                type='checkbox' name={`select-${item.id}`}
-                onChange={(e) => handleSingleCheck(e.target.checked, item.id)}
-                // 체크된 아이템 배열에 해당 아이템이 있을 경우 선택 활성화, 아닐 시 해제, 장바구니에서 미리 선택했던 상품에 대해 체크를 해두기 위함
-                checked={checkItems.includes(item.id) ? true : false} 
-              />
-              <img src={item.img} alt='이미지'/>
-              <span className={styles.itemInfo}>
-                
-                <div>
-                  <p>{item.name}</p>
-                  <p>옵션 : {item.option1} / {item.option2}</p>
-                </div>
-                <div>
-                  <div>{item.price}원</div>
-                  <div>{item.count}개</div>
-                </div>
-                
-                <div>{item.price * item.count}원</div>
-                <div>
-                  <button>삭제</button>
-                </div>
-              </span>
-          </div>
             ))} */}
-          </div>
-          {/* 주문하기버튼 */}
-          <div className={styles.button_box}>
-            <button onClick={()=>{setPaymentModal(!paymentModal)}}>
-              주문하기
-            </button>
-          </div>
-        </div>
-        <Footer/>
-      </div>
-    </>
-    
-  )
-}
-
-export default Basket
